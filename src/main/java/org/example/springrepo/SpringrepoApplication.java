b9d5f480395348fa77012a78155292c7682d5a48
divesh.kumar <divesh.kumar@clevertap.com>
2025-03-22 13:19:29 +0530

    project init

---
+++ src/main/java/org/example/springrepo/SpringrepoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springrepo/SpringrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
